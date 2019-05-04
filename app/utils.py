587e03907c97e5acbcd1251e99e345b9fb6e26c9
seeni <seeni0424@gmail.com>
2019-05-04 14:45:04 +0530

    added better search needed to implement ifdf

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,14 @@
 from app.models import Organization
+from math import log2
 
 def searchMission(mission):
 	result={"orgs":[],"len":0}
-	for org in Organization.query.all():
-		if org.mission==mission:
-			result["orgs"].append(org)
-			result["len"]+=1
+	orgs = Organization.query.all()
+	for org in orgs:
+		org_mission = org.mission
+		for word in org_mission.split(" "):
+			if word in mission.split(" "):
+				result["orgs"].append(org)
+				result["len"]+=1
+				break
 	return result
